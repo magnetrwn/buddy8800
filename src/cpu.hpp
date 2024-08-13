2c5a846eb52335680f5ed27e5c29f758dfc24f64
magnetrwn <rpolli.dev@tutanota.com>
2024-08-13 12:05:45 +0200

    Added cpu state flags.

--- src/cpu.hpp
+++ src/cpu.hpp
@@ -4,47 +4,90 @@
 #include <array>
 #include "typedef.hpp"
 
-enum cpu_registers8 {
+/// @brief Enumerates the 8 bit registers of the CPU (matches cpu_registers16).
+enum class cpu_registers8 {
     A, F, B, C, D, E, H, L, HIGH_SP, LOW_SP, HIGH_PC, LOW_PC
 };
 
-enum cpu_registers16 {
+/// @brief Enumerates the 16 bit registers of the CPU (matches cpu_registers8).
+enum class cpu_registers16 {
     AF, BC, DE, HL, SP, PC
 };
 
+/// @brief Enumerates the flags of the CPU at their bit position.
+enum class cpu_flags {
+    C = 0x01, P = 0x04, AC = 0x10, Z = 0x40, S = 0x80
+};
+
+/**
+ * @brief Represents the state of the CPU.
+ * 
+ * This struct represents the CPU state and provides useful compile-time methods to work with it.
+ * On construction all register space is initialized to zero.
+ */
 struct cpu_state {
-    // AF, BC, DE, HL, SP, PC
     std::array<u16, 6> registers;
     cpu_state() : registers({}) {}
 
+    /// @name Register state get/setters.
+    /// \{
+
+    /// @brief Get the value of an 8 bit register (including halves of SP and PC).
     template <cpu_registers8 reg>
     constexpr u8 get_register8() const {
-        if constexpr (reg & 1)
-            return registers[reg >> 1] & 0xFF;
+        if constexpr (static_cast<usize>(reg) & 1)
+            return registers[static_cast<usize>(reg) >> 1] & 0xFF;
         else
-            return registers[reg >> 1] >> 8;
+            return registers[static_cast<usize>(reg) >> 1] >> 8;
     }
 
+    /// @brief Get the value of a 16 bit register (including any pair of 8 bit registers).
     template <cpu_registers16 pair>
     constexpr u16 get_register16() const {
-        return registers[pair];
+        return registers[static_cast<usize>(pair)];
     }
 
+    /// @brief Set the value of an 8 bit register (including halves of SP and PC).
     template <cpu_registers8 reg>
     constexpr void set_register8(u8 value) {
-        if constexpr (reg & 1) {
-            registers[reg >> 1] &= 0xFF00;
-            registers[reg >> 1] |= value;
+        if constexpr (static_cast<usize>(reg) & 1) {
+            registers[static_cast<usize>(reg) >> 1] &= 0xFF00;
+            registers[static_cast<usize>(reg) >> 1] |= value;
         } else {
-            registers[reg >> 1] &= 0x00FF;
-            registers[reg >> 1] |= (value << 8);
+            registers[static_cast<usize>(reg) >> 1] &= 0x00FF;
+            registers[static_cast<usize>(reg) >> 1] |= (value << 8);
         }
     }
 
+    /// @brief Set the value of a 16 bit register (including any pair of 8 bit registers).
     template <cpu_registers16 pair>
     constexpr void set_register16(u16 value) {
-        registers[pair] = value;
+        registers[static_cast<usize>(pair)] = value;
     }
+
+    /// \}
+    /// @name Flag state get/set/unsetters.
+    /// \{
+
+    /// @brief Get the value of a flag.
+    template <cpu_flags flag>
+    constexpr bool get_flag() const {
+        return (get_register8<cpu_registers8::F>() & static_cast<u8>(flag)) != 0x00;
+    }
+
+    /// @brief Set a flag.
+    template <cpu_flags flag>
+    constexpr void set_flag() {
+        return set_register8<cpu_registers8::F>(get_register8<cpu_registers8::F>() | static_cast<u8>(flag));
+    }
+
+    /// @brief Unset a flag.
+    template <cpu_flags flag>
+    constexpr void unset_flag() {
+        return set_register8<cpu_registers8::F>(get_register8<cpu_registers8::F>() & ~static_cast<u8>(flag));
+    }
+
+    /// \}
 };
 
 #endif
--- tests/test_cpu.hpp
+++ tests/test_cpu.hpp
@@ -7,6 +7,9 @@ TEST_CASE("CPU state registers.", "[cpu_state]") {
     cpu_state state;
 
     SECTION("Testing get and set working in-between 8 and 16 bit requests.") {
+        using enum cpu_registers8;
+        using enum cpu_registers16;
+
         // Get and set on 8 bit registers
         state.set_register8<A>(0x12);
         state.set_register8<F>(0x34);
@@ -27,6 +30,9 @@ TEST_CASE("CPU state registers.", "[cpu_state]") {
     }
 
     SECTION("Testing get and set not modifying unrelated register space.") {
+        using enum cpu_registers8;
+        using enum cpu_registers16;
+        
         // Set all register pairs to a known value using 16 bit setters
         state.set_register16<AF>(0x0000);
         state.set_register16<BC>(0x5555);
@@ -66,5 +72,43 @@ TEST_CASE("CPU state registers.", "[cpu_state]") {
         REQUIRE(state.get_register16<HL>() == 0x7898);
         REQUIRE(state.get_register16<SP>() == 0x9A76);
         REQUIRE(state.get_register16<PC>() == 0xBC54);
+    }
+
+    SECTION("Testing getting, setting, unsetting flags.") {
+        using enum cpu_flags;
+
+        // Set all flags to 1 via set_register8
+        state.set_register8<cpu_registers8::F>(0xFF);
+
+        // Check that all flags are set via get_flag
+        REQUIRE(state.get_flag<C>());
+        REQUIRE(state.get_flag<P>());
+        REQUIRE(state.get_flag<AC>());
+        REQUIRE(state.get_flag<Z>());
+        REQUIRE(state.get_flag<S>());
+
+        // Unset all flags via unset_flag
+        state.unset_flag<C>();
+        state.unset_flag<P>();
+        state.unset_flag<AC>();
+        state.unset_flag<Z>();
+        state.unset_flag<S>();
+
+        // Check that all flags are unset via get_register8
+        REQUIRE(!state.get_flag<C>());
+        REQUIRE(!state.get_flag<P>());
+        REQUIRE(!state.get_flag<AC>());
+        REQUIRE(!state.get_flag<Z>());
+        REQUIRE(!state.get_flag<S>());
+
+        // Set all flags to 1 via set_flag
+        state.set_flag<C>();
+        state.set_flag<P>();
+        state.set_flag<AC>();
+        state.set_flag<Z>();
+        state.set_flag<S>();
+
+        // Check that all flags are set via get_register8
+        REQUIRE(state.get_register8<cpu_registers8::F>() == 0xFF);
     }
 }
